760d0f9ef86c9eaca898bf486fd6b8f7f3d698ac
jessica <ishimwejessica2@gmail.com>
2025-08-06 16:50:41 +0200

    adding reduce.js & convert.js file

---
+++ 8kyu/convert.js
@@ -0,0 +1,6 @@
+//Complete the function which converts hex number (given as a string) to a decimal number.
+
+function hexToDec(hexString) {
+    return parseInt(hexString, 16);
+}
+console.log(hexToDec('ff01a'));
